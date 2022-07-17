c4ff4f548bc16531c0c247dae0adcd29fb605f6a
Yusra Abdi <yusra4411@gmail.com>
2022-07-17 09:50:44 -0700

    addded files

--- js/main.js
+++ js/main.js
@@ -1,18 +1,5 @@
 
 //Menu 
-const menu = document.querySelector(".menu-list");
-const menuBtn = document.querySelector(".menu-btn");
-const cancelBtn = document.querySelector(".cancel-btn");
-menuBtn.onclick = ()=>{
-  menu.classList.add("active");
-  menuBtn.classList.add("hide");
-}
-
-cancelBtn.onclick = ()=>{
-  menu.classList.remove("active");
-  menuBtn.classList.remove("hide");
-}
-
 
 
 //FILTER TABS
