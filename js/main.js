917c287206792a1f616e039001e73dca63d29069
Yusra Abdi <yusra4411@gmail.com>
2022-07-18 07:36:17 -0700

    addded files

--- js/main.js
+++ js/main.js
@@ -1,92 +1,91 @@
 
-//Menu 
-
-
 //FILTER TABS
+
+  
 const showAllTab = document.querySelector('.all');
- const hotTab = document.querySelector('.hot-drink')
- const shakeTab = document.querySelector('.milkshake')
+const hotTab = document.querySelector('.hot-drink')
+const shakeTab = document.querySelector('.milkshake')
 
- const coldTab = document.querySelector('.cold-drink')
- const dessertTab = document.querySelector('.dessert');
- const frappeTab = document.querySelector('.frappe')
- const drinkTab = document.querySelector('.drink')
+const coldTab = document.querySelector('.cold-drink')
+const dessertTab = document.querySelector('.dessert');
+const frappeTab = document.querySelector('.frappe')
+const drinkTab = document.querySelector('.drink')
 
-const allFilteredP = document.querySelectorAll('.filter-p')
+const allFilteredDiv = document.querySelectorAll('.filter-div')
 
 // Turns node list into an array
-const allFilteredPArray = Array.from(allFilteredP)
+const allFilteredDivArray = Array.from(allFilteredDiv)
 
 showAllTab.addEventListener('click', showAll)
 function showAll() {
-  allFilteredPArray.forEach(p =>{
-    p.classList.remove('remove');
-    p.classList.add('show')
-  })
+ allFilteredDiv.forEach(p =>{
+   p.classList.remove('remove');
+   p.classList.add('show')
+ })
 }
 
 hotTab.addEventListener('click', showHotDrink)
 function showHotDrink() {
-  allFilteredPArray.forEach(p =>{
-    p.classList.remove('remove');
-    
-    if(!p.dataset.hotdrink){
-         p.classList.add('remove');
+ allFilteredDiv.forEach(p =>{
+   p.classList.remove('remove');
+   
+   if(!p.dataset.hotdrink){
+        p.classList.add('remove');
 
-    }
-  })
+   }
+ })
 }
 
 coldTab.addEventListener('click', showColdDrink)
 function showColdDrink() {
-  allFilteredPArray.forEach(p =>{
-    p.classList.remove('remove');
-    
-    if(!p.dataset.colddrink){
-         p.classList.add('remove');
+ allFilteredDiv.forEach(p =>{
+   p.classList.remove('remove');
+   
+   if(!p.dataset.colddrink){
+        p.classList.add('remove');
 
-    }
-  })
+   }
+ })
 }
 shakeTab.addEventListener('click', showShake)
 function showShake() {
-  allFilteredPArray.forEach(p => {
-    p.classList.remove('remove');
-    
-    if(!p.dataset.milkshake) {
-      p.classList.add('remove')
-    }
-  })
+ allFilteredDiv.forEach(p => {
+   p.classList.remove('remove');
+   
+   if(!p.dataset.milkshake) {
+     p.classList.add('remove')
+   }
+ })
 }
 dessertTab.addEventListener('click', showDessert)
 function showDessert() {
-  allFilteredPArray.forEach(p => {
-    p.classList.remove('remove');
-    
-    if(!p.dataset.dessert) {
-      p.classList.add('remove')
-    }
-  })
+ allFilteredDiv.forEach(p => {
+   p.classList.remove('remove');
+   
+   if(!p.dataset.dessert) {
+     p.classList.add('remove')
+   }
+ })
 }
 
 frappeTab.addEventListener('click', showFrappe)
 function showFrappe() {
-  allFilteredPArray.forEach(p => {
-    p.classList.remove('remove');
-    
-    if(!p.dataset.frappe) {
-      p.classList.add('remove')
-    }
-  })
+ allFilteredDiv.forEach(p => {
+   p.classList.remove('remove');
+   
+   if(!p.dataset.frappe) {
+     p.classList.add('remove')
+   }
+ })
 }
 
 drinkTab.addEventListener('click', showDrink)
 function showDrink() {
-  allFilteredPArray.forEach(p => {
-    p.classList.remove('remove');
-    
-    if(!p.dataset.drink) {
-      p.classList.add('remove')
-    }
-  })
+ allFilteredDiv.forEach(p => {
+   p.classList.remove('remove');
+   
+   if(!p.dataset.drink) {
+     p.classList.add('remove')
+   }
+ })
 }
